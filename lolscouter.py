bd577b0bf6946259ea075c9249f66b1689eef86e
r3cursive <kirk@imkirk.com>
2014-07-15 19:31:46 -0700

    syncing todo

--- lolscouter.py
+++ lolscouter.py
@@ -11,3 +11,6 @@ scoutTeam = [ "r3cursive",
 cparser = ConfigParser.ConfigParser()
 cparser.read('config.ini')
 lolapi = LeagueOfLegends(cparser.get('lol','API_KEY'))
+
+#TODO: Pull daily from Elophant, calculate drift based on that?
+#TODO: Other ways to pull match history?
