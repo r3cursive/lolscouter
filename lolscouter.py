918b857ad57064a02bc4d07b75a910189497c4bd
r3cursive <kirk@imkirk.com>
2014-07-22 06:03:15 -0700

    fleshed out elophant parser, added some gdocs stuff

--- lolscouter.py
+++ lolscouter.py
@@ -1,6 +1,7 @@
 import ConfigParser
-
-scoutTeam = [ "r3cursive",
+import pyleague
+import gspread
+scout_team = [ "r3cursive",
     "hollowbandit",
     "monchoon",
     "a slippery stick",
@@ -10,7 +11,39 @@ scoutTeam = [ "r3cursive",
 
 cparser = ConfigParser.ConfigParser()
 cparser.read('config.ini')
-lolapi = LeagueOfLegends(cparser.get('lol','API_KEY'))
+lolapi = pyleague.PyLeague(cparser.get('lol', 'api_key'))
+gs = gspread.login(cparser.get('google', 'username'),cparser.get('google', 'password'))
 
-#TODO: Pull daily from Elophant, calculate drift based on that?
-#TODO: Other ways to pull match history?
+sheet = gs.open('ScoutSheet')
+
+teamdict = {}
+for user in scout_team:
+    user_matches = lolapi.get_elophant_match_history(lolapi.get_summoner_by_name(user))
+    try:
+        usersheet = sheet.worksheet(user)
+    except gspread.WorksheetNotFound:
+        usersheet = sheet.add_worksheet(title=user, rows=500, cols=20)
+        usersheet = sheet.worksheet(user)
+
+    usersheet.update_cell(0, 0, 'champion')
+    usersheet.update_cell(1, 0, 'outcome')
+    usersheet.update_cell(2, 0, 'length')
+    usersheet.update_cell(3, 0, 'kills')
+    usersheet.update_cell(4, 0, 'deaths')
+    usersheet.update_cell(5, 0, 'assists')
+    usersheet.update_cell(6, 0, 'kda')
+    usersheet.update_cell(7, 0, 'id')
+    # i dont know a better way to track these :(
+    sheet_Y = 1
+    import ipdb; ipdb.set_trace()
+    for key in user_matches.keys():
+        for match in user_matches[key]:
+            usersheet.update_cell(0, sheet_Y, match['champion'])
+            usersheet.update_cell(1, sheet_Y, match['outcome'])
+            usersheet.update_cell(2, sheet_Y, match['length'])
+            usersheet.update_cell(3, sheet_Y, match['kills'])
+            usersheet.update_cell(4, sheet_Y, match['deaths'])
+            usersheet.update_cell(5, sheet_Y, match['assists'])
+            usersheet.update_cell(6, sheet_Y, match['kda-stats'])
+            usersheet.update_cell(7, sheet_Y, match['id'])
+            sheet_Y += 1
--- pyleague.py
+++ pyleague.py
@@ -1,10 +1,10 @@
 """
-Python Library for League of Legends
+Python Library for League of Legends with elophant match history
 """
 __author__ = 'r3cursive'
 import json
 import urllib
-
+from lxml import etree
 
 class PyLeague:
     api_endpoint = 'https://na.api.pvp.net/api/lol'
@@ -15,7 +15,7 @@ class PyLeague:
         self.api_reigon = api_reigon
         self.api_url = self.api_endpoint + '/' + api_reigon + '/' + 'v' + api_version + '/'
 
-    def summoner_by_name(self, summoner_name=''):
+    def get_summoner_by_name(self, summoner_name=''):
         if summoner_name == '':
             return
         url = self.api_url + 'summoner/by-name/%s?api_key=%s' % (summoner_name, self.api_key)
@@ -30,3 +30,47 @@ class PyLeague:
         for summoner in summoner_names:
             summoner_array.append(self.summoner_by_name(summoner))
         return summoner_array
+
+    def get_elophant_match_history(self, summoner_id):
+        if not summoner_id:
+            return
+        etparser = etree.HTML(urllib.urlopen('http://www.elophant.com/league-of-legends/summoner/na/%s/recent-games' %
+                                             summoner_id).read())
+        recent_boxes = etparser.xpath('//div[@id="tab-recent-matches"]/div[@class="box"]')
+        champhist = {}
+        for box in recent_boxes:
+            match = {}
+            #TODO: ignore if match isn't ranked / normal / teambuilder
+            match['champion'] = box.xpath('./div/div[@class="pic"]/a/div[@class="title"]')[0]\
+                .text
+            match['outcome'] = \
+                box.xpath('./div/div[@class="desc self-clear"]/div[@class="game-info"]/div/span')[0]\
+                    .text
+            match['length'] = \
+                box.xpath('./div/div[@class="desc self-clear"]/div[@class="game-info"]/div/span')[1]\
+                    .text
+            match['kills'] = \
+                box.xpath('./div/div[@class="desc self-clear"]/div[@class="kda-stats"]/div/div[@class="kills"]/span')[0]\
+                    .text.split(' ')[0]
+            match['deaths'] = \
+                box.xpath('./div/div[@class="desc self-clear"]/div[@class="kda-stats"]/div/div[@class="deaths"]/span')[0]\
+                    .text.split(' ')[0]
+            match['assists'] = \
+                box.xpath('./div/div[@class="desc self-clear"]/div[@class="kda-stats"]/div/div[@class="assists"]/span')[0]\
+                    .text.split(' ')[0]
+            try:
+                match['kda-stats'] = \
+                    float((int(match['kills'])+int(match['assists']))/int(match['deaths']))
+            except ZeroDivisionError:
+                match['kda-stats'] = int(match['kills'])+int(match['assists'])
+
+            match['id'] = hash(frozenset(match))
+
+            if match['champion'] in champhist:
+                champhist[match['champion']].append(match)
+            else:
+                matcharray = []
+                matcharray.append(match)
+                champhist[match['champion']] = matcharray
+
+        return champhist
